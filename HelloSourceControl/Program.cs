d62eae86a3d8aca125bf57c73e2b2c906cf8e9b9
bzeller7 <zeller8964@student.cptc.edu>
2019-01-02 14:44:44 -0800

    Added Console.WriteLine

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -10,6 +10,8 @@ namespace HelloSourceControl
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
